195f95910c7669a3a60d7c08c93b3206ab0fe3d9
valdriano oliveira da costa <valdriano.oliveira@gmail.com>
2026-07-01 14:06:10 -0400

    ajuste de styles

--- ValPlay/Converters/SelectionConverters.cs
+++ ValPlay/Converters/SelectionConverters.cs
@@ -27,3 +27,12 @@ public sealed class SelectionCheckOpacityConverter : IValueConverter
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         => throw new NotSupportedException();
 }
+
+public sealed class SelectionIconConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is true ? "☑" : "☐";
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
